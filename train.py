ece8866825dd73297ea17bb164b0107b2740728d
mztom3745 <103809026+mztom3745@users.noreply.github.com>
2025-05-27 22:07:11 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import argparse
 import torch
 from model import UNetModel  
-from flowmatching import get_dataloader, train 
+from flow_matching import get_dataloader, train 
 
 def main():
     # 解析命令行参数
@@ -9,6 +9,7 @@ def main():
     parser.add_argument("--save_dir", type=str, default="/content/drive/MyDrive/zsz", help="Directory to save checkpoints")
     parser.add_argument("--model_name", type=str, default="unet_fm", help="Model name for checkpointing")
     parser.add_argument("--batch_size", type=int, default=256, help="Batch size for training")
+    parser.add_argument("--data_path", type=str, default="/root/autodl-pub/cifar-10", help="Directory to data")
 
     args = parser.parse_args()
 
@@ -17,7 +18,7 @@ def main():
 
     # 初始化模型和数据加载器
     model = UNetModel().to(device)
-    dataloader = get_dataloader(batch_size=args.batch_size)
+    dataloader = get_dataloader(batch_size=args.batch_size,data_path=args.data_path)
 
     # 训练模型
     train(
